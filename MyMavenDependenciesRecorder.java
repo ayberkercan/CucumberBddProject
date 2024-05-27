8aec063377718828473f12b9db2d449ede1a9c2c
ayberk.ercan <ayberk.ercan@turkuvazyayin.com.tr>
2024-05-27 11:33:06 +0300

    t1slljmjmkıfı1sqeee

---
+++ MyMavenDependenciesRecorder.java
@@ -0,0 +1,7 @@
+package com.yourcompany.jenkins;
+
+import org.jfrog.hudson.maven2.MavenDependenciesRecorder;
+
+public class MyMavenDependenciesRecorder extends MavenDependenciesRecorder {
+    // Anonim sınıfın içeriği buraya taşınacak
+}
